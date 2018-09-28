f0e4f0f861a729b4e59ac48a5ecdfb46153e5af3
AC De Leon <aadeleon@sdsu.edu>
2018-09-27 17:50:39 -0700

    [+] Redux dummy data for ProjectList

--- app/src/components/projects/ProjectList.js
+++ app/src/components/projects/ProjectList.js
@@ -1,31 +1,14 @@
 import React from "react";
+import ProjectSummary from "./ProjectSummary";
 
-const ProjectList = () => {
+const ProjectList = ({ projects }) => {
   return (
     <div className="project-list section">
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by @acfromspace</p>
-          <p className="grey-text">3rd September, 3pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by @acfromspace</p>
-          <p className="grey-text">3rd September, 3pm</p>
-        </div>
-      </div>
-
-      <div className="card z-depth-0 project-summary">
-        <div className="card-content grey-text text-darken-3">
-          <span className="card-title">Project Title</span>
-          <p>Posted by @acfromspace</p>
-          <p className="grey-text">3rd September, 3pm</p>
-        </div>
-      </div>
+      {/* Instead of listing each summary, just map out the projects listed */}
+      {projects &&
+        projects.map(project => {
+          return <ProjectSummary project={project} key={project.id} />;
+        })}
     </div>
   );
 };
--- app/src/store/reducers/projectReducer.js
+++ app/src/store/reducers/projectReducer.js
@@ -1,4 +1,10 @@
-const initState = {};
+const initState = {
+  projects: [
+    { id: "1", title: "help me find peach", content: "blah blah blah" },
+    { id: "2", title: "help me bowsette", content: "blah blah blah" },
+    { id: "3", title: "help me boi", content: "blah blah blah" }
+  ]
+};
 
 const projectReducer = (state = initState, action) => {
   return state;
