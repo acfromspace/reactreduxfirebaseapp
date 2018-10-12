db2781b7917ed49ba884a4fd375df92820188878
AC De Leon <aadeleon@sdsu.edu>
2018-10-11 18:25:48 -0700

    [*] yarn, fixing semi-error

--- app/src/store/reducers/projectReducer.js
+++ app/src/store/reducers/projectReducer.js
@@ -12,6 +12,9 @@ const projectReducer = (state = initState, action) => {
   switch (action.type) {
     case "CREATE_PROJECT":
       console.log("created project", action.project);
+      break;
+    default:
+      break;
   }
   return state;
 };
