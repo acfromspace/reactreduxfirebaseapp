eef6eab825ab0c08fef87edbb2318c955756cecd
AC De Leon <aadeleon@sdsu.edu>
2018-10-11 18:44:42 -0700

    [+] React, Redux, Firebase, Firestore CONNECTED

--- app/src/index.js
+++ app/src/index.js
@@ -7,12 +7,22 @@ import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./store/reducers/rootReducer";
 import { Provider } from "react-redux";
 import thunk from "redux-thunk";
+import { reduxFirestore, getFirestore } from "redux-firestore";
+import { reactReduxFirebase, getFirebase } from "react-redux-firebase";
+import firebaseConfig from "./config/firebaseConfig";
 
 // Redux
 
 // applyMiddleware = a store enhancer
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(
+  rootReducer,
+  compose(
+    applyMiddleware(thunk.withExtraArgument({ getFirestore, getFirebase })),
+    reduxFirestore(firebaseConfig),
+    reactReduxFirebase(firebaseConfig)
+  )
+);
 
 ReactDOM.render(
   <Provider store={store}>
