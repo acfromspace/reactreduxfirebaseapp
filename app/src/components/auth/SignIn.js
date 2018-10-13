db7a850f223626fe32548c6d66c4b12ceeaff7b1
AC De Leon <aadeleon@sdsu.edu>
2018-10-12 22:35:52 -0700

    [+] Login credentials

--- app/src/components/auth/SignIn.js
+++ app/src/components/auth/SignIn.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import { signIn } from "../../store/actions/authActions";
 
 class SignIn extends Component {
   state = { email: "", password: "" };
@@ -10,10 +12,11 @@ class SignIn extends Component {
   };
   handleSubmit = e => {
     e.preventDefault();
-    console.log(this.state);
+    this.props.signIn(this.state);
   };
 
   render() {
+    const { authError } = this.props;
     return (
       <div className="container">
         <form onSubmit={this.handleSubmit} className="white">
@@ -28,6 +31,10 @@ class SignIn extends Component {
           </div>
           <div className="input-field">
             <button className="btn pink lighten-1 z-depth-0">Login</button>
+            <div className="red-text center">
+              {/* Show error if only error exists */}
+              {authError ? <p>{authError}</p> : null}
+            </div>
           </div>
         </form>
       </div>
@@ -35,4 +42,20 @@ class SignIn extends Component {
   }
 }
 
-export default SignIn;
+const mapStateToProps = state => {
+  return {
+    authError: state.auth.authError
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    signIn: creds => dispatch(signIn(creds))
+  };
+};
+
+// First argument is null due to it not being mapStateToProps, so we do the 2nd argument of mapDispatchToProps
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(SignIn);
