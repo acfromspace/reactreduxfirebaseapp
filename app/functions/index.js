44c54a96afa1ed2108a237fa180047d886f0932b
AC De Leon <aadeleon@sdsu.edu>
2018-10-23 17:54:38 -0700

    [+] MORE CLOUDS!

--- app/functions/index.js
+++ app/functions/index.js
@@ -1,9 +1,53 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
 exports.greetingsMessage = functions.https.onRequest((request, response) => {
   response.send("Hello MY BOI!");
+});
+
+const createNotification = notification => {
+  return admin
+    .firestore()
+    .collection("notifications")
+    .add(notification)
+    .then(doc => console.log("Notification added!", doc));
+};
+
+exports.projectCreated = functions.firestore
+  // Reference a document in the projects collection via id
+  .document("projects/{projectId}")
+  // When document is created then...
+  .onCreate(doc => {
+    // All document data is in the project variable
+    const project = doc.data();
+    const notification = {
+      content: "Added a new project!",
+      user: `${project.authorFirstName} ${project.authorLastName}`,
+      time: admin.firestore.FieldValue.serverTimestamp()
+    };
+
+    // All cloud functions expect some sort of response
+    return createNotification(notification);
+  });
+
+exports.userJoined = functions.auth.user().onCreate(user => {
+  return admin
+    .firestore()
+    .collection("users")
+    .doc(user.uid)
+    .get()
+    .then(doc => {
+      const newUser = doc.data();
+      const notification = {
+        content: "Joined the party!",
+        user: `${newUser.firstName} ${newUser.lastName}`,
+        time: admin.firestore.FieldValue.serverTimestamp()
+      };
+
+      return createNotification(notification);
+    });
 });
